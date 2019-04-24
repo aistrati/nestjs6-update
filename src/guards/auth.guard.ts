f0c5f9f398a8b59dd6dc9ccef3adf3eeb0ff94bb
Andrei <andrei.istrati@vitamojo.com>
2019-04-24 16:59:12 +0300

    first commit

---
+++ src/guards/auth.guard.ts
@@ -0,0 +1,9 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    return false;
+  }
+}
---
+++ test/app.e2e-spec.ts
@@ -0,0 +1,27 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+import { AuthGuard } from '../src/guards/auth.guard';
+
+describe('AppController (e2e)', () => {
+  let app;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    })
+      .overrideGuard(AuthGuard)
+      .useValue({ canActivate: () => true })
+      .compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('/ (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/')
+      .expect(200)
+      .expect('Hello World!');
+  });
+});
